c2a36ff7823c7aebc7151a680f7f60621f64faca
Vishnubhotla V D V Bharadwaj <vbharadwaj3456@gmail.com>
2023-11-13 17:04:10 -0500

    Time: 30 ms (74.89%), Space: 14.5 MB (38.89%) - LeetHub

---
+++ 0242-valid-anagram/0242-valid-anagram.py
@@ -0,0 +1,11 @@
+class Solution(object):
+    def isAnagram(self, s, t):
+        """
+        :type s: str
+        :type t: str
+        :rtype: bool
+        """
+        a =sorted(s)
+        b =sorted(t)
+        return a==b
+        
